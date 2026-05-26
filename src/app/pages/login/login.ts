5e64023b8900b49c6562a44e2ace8644605e6d38
Verly1969 <eddyverly@icloud.com>
2026-05-26 21:42:10 +0200

    Mise en place du formulaire de login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
+  isLoading = false;
+  showPassword = false;
+  errorMessage = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+
+    // Initialisation du formulaire avec validation
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [Validators.required, Validators.email]
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(8)]
+      ]
+    });
+  }
+
+  // Vérifier si un champ est invalid et touché
+  isFieldInvalid(field: string): boolean {
+    const control = this.loginForm.get(field);
+    return !!(control?.invalid && control?.touched);
+  }
+
+  // Afficher / cacher le mot de passe
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  // Soumission du formulaire
+  onSubmit(): void {
+    if(this.loginForm.invalid) return;
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    const { email, password } = this.loginForm.value;
+
+    // simulation d'une connexion
+    setTimeout(() => {
+      this.isLoading = false;
+
+      if(email === 'admin@test.com' && password === 'password123')
+      {
+        this.router.navigate(['/dashboard']);
+      }
+      else
+      {
+        this.errorMessage = 'Email ou mot de passe incorrect';
+      }
+    }, 1000);
+  }
 
 }
